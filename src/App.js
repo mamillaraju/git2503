e761c26f2d1aeba4a81b62c787a9d2593ceeb92f
mamillaraju <mamillaraju8@gmail.com>
2025-06-09 01:49:32 +0530

    i have written definations

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ function App() {
     <h2>hello world</h2>
     <h2>i have worked on push pull clone</h2>
 
+    <p>Git push allows us to transfer files from the local repository to remote repository hosting services like GitHub, GitLab, etc. Other developers who want to work on the files can access them after being uploaded to a remote repository.</p>
+
+    <p>The git clone command creates a copy of an existing Git repository. This repository can be hosted on platforms like GitHub, GitLab, or Bitbucket, or it can reside on a local or remote server. Cloning a repository involves copying all the repository data to your local machine, including the entire history and all branches.</p>
+
+    <p>Pull requests (PRs) are a fundamental part of collaborative development in Git and are widely used across platforms like GitHub, GitLab, and Bitbucket. A pull request is a mechanism that allows developers to notify others about changes they have made to a branch in a Git repository.</p>
+
     </div>
   );
 }
